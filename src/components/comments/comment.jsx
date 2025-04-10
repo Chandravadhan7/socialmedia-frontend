e16e9bd64129d403f9e72a23359053b4dd69f6f7
Chandravadhan <m.chandravadhan@gmail.com>
2025-04-10 23:43:16 +0530

    implemented function for fetching user details,

--- src/components/comments/comment.jsx
+++ src/components/comments/comment.jsx
@@ -8,6 +8,8 @@ export default function Comment({ comment }) {
   let [replies, setReplies] = useState([]);
   let [showReplies, setShowReplies] = useState(false);
   let [reply, setReply] = useState("");
+  const [userDetails,setUserDetails] = useState(null);
+
 
   const getRelativeTime = (epoch) => {
     return formatDistanceToNowStrict(new Date(epoch), { addSuffix: true });
@@ -59,21 +61,45 @@ export default function Comment({ comment }) {
     setReply("");
   };
 
+  const getUser = async () => {
+        const response = await fetch(`http://localhost:8080/user/${comment?.userId}`,{
+            method:"GET",
+            headers:{
+                sessionId:sessionId,
+                userId:userId
+            }
+        });
+  
+        if(!response.ok){
+            throw new Error("failed to fetch user details");
+        }
+  
+        const userResponse = await response.json();
+        setUserDetails(userResponse);
+        console.log(userResponse);
+    }
+  
+    useEffect(() => {
+      getUser();
+    }, []);
+
   return (
    <div className="whole-cont">
     <div className="comment-cont">
       <div className="comment-cont-s1">
-        <div className="comment-cont-s1-img-cont"></div>
+        <div className="comment-cont-s1-img-cont">
+        <img src={"https://i.ibb.co/67HWYXmq/icons8-user-96.png"} className="post-pro-pic" alt="profile"/>
+        </div>
       </div>
       <div className="comment-cont-s2">
         <div className="comment-cont-s2-1">
           <div className="comm-username">
-            {/* Display username if available */}
+           <div style={{fontSize:"105%",fontWeight:"555"}}>{userDetails?.name}</div> 
+            <div className="com-time">{getRelativeTime(comment?.createdAt)}</div>
           </div>
           <div className="comm-content">{comment?.content}</div>
         </div>
         <div className="comment-cont-s2-2">
-          <div className="com-time">{getRelativeTime(comment?.createdAt)}</div>
           <div className="com-like">like</div>
           <div
             className="com-reply"
@@ -88,7 +114,7 @@ export default function Comment({ comment }) {
     </div>
     {showReplies && (
         <div className="reply-section">
-          <div className="reply-child">
+          <div className={replies.length > 0?"reply-child":"d-none"}>
             {replies.map((item) => {
                 return(
                     <Comment comment={item}/>
@@ -96,14 +122,20 @@ export default function Comment({ comment }) {
             })}
           </div>
           <div className="add-reply">
-          <div className="pic-cont"></div>
-            <input placeholder="reply to user" onChange={(e)=>setReply(e.target.value)} value={reply} className="comment-bar"/>
+          <div className= "pic-cont">
+          <img
+              src={"https://i.ibb.co/67HWYXmq/icons8-user-96.png"}
+              className="post-pro-pic"
+              alt="profile"
+            />
+          </div>
+          <input placeholder="reply to user" onChange={(e)=>setReply(e.target.value)} value={reply} className="comment-bar"/>
             <button onClick={() => { 
             addReply(); 
             setTimeout(() => {
             window.location.reload();
             }, 500);
-            }}>Post</button>
+            }} className="reply-btn">Post</button>
           </div>
         </div>
       )}
