1ad3151e36d91aab8f59de72c90138e15274882a
Chandravadhan <m.chandravadhan@gmail.com>
2025-04-22 23:25:29 +0530

    Implement relative time function to display message timestamp from epoch

--- src/components/message/message.jsx
+++ src/components/message/message.jsx
@@ -1,15 +1,29 @@
+import { formatDistanceToNowStrict } from "date-fns";
 import "./message.css"
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
 export default function Message({ message, isUserMessage, style }) {
+    const getRelativeTime = (epoch) => {
+    return formatDistanceToNowStrict(new Date(epoch), { addSuffix: true });
+  };
+
     return (
         <div
             className="convo-page-side2-convo-msg"
             style={{
                 ...style,
-                backgroundColor: isUserMessage ? "#e1ffc7" : "#ffffff",
-                borderColor: isUserMessage ? "green" : "blue",
+                backgroundColor: isUserMessage ? "#3B82F6" : "#1F2A3C",
+                borderColor: isUserMessage ? "green" : "#152135",
             }}
         >
-            {message?.content}
+            <div className="convo-page-side2-convo-msg-msg">{message?.content}</div>
+            <div className="convo-page-side2-convo-msg-time">
+                <div className="time">
+                    {getRelativeTime(message?.createdAt)}
+                </div>
+                <div className="tick">
+                <DoneAllOutlinedIcon sx={{ fontSize: "15px" }} />
+                </div>
+            </div>
         </div>
     );
 }
