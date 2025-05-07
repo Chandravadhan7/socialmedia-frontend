23dc6a2b8bdc4363dceee34fdde174fa44264747
Chandravadhan <m.chandravadhan@gmail.com>
2025-05-07 23:45:00 +0530

    Add user profile page with mutual friends, bio, posts, and conversation functionality

--- src/pages/userprofile.jsx
+++ src/pages/userprofile.jsx
@@ -1,97 +1,435 @@
-import { useEffect, useState } from "react"
-import "./userprofile.css"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./userprofile.css";
 import Post from "../components/post/post";
-export default function UserProfile({user_id}){
-    let [posts,setPosts] = useState([]);
-    let sessionId = localStorage.getItem("sessionId");
-    let userId = localStorage.getItem("userId");
-    let [userDetails,setUserDetails] = useState(null);  
+import { SiWorkplace } from "react-icons/si";
+import { RiSchoolLine } from "react-icons/ri";
+import { LiaUniversitySolid } from "react-icons/lia";
+import { IoHomeOutline } from "react-icons/io5";
+import { CiLocationOn } from "react-icons/ci";
+import { GiRelationshipBounds } from "react-icons/gi";
+import { PiDotsThreeBold } from "react-icons/pi";
 
-    const getPosts = async () =>{
-        const response = await fetch(`http://localhost:8080/post/posts/${user_id}`,{
-            method:"GET",
-            headers:{
-                sessionId: sessionId,
-                userId: userId 
-            }
-        });
+export default function UserProfile({ user_id }) {
+  const [posts, setPosts] = useState([]);
+  const [userDetails, setUserDetails] = useState(null);
+  const sessionId = localStorage.getItem("sessionId");
+  const userId = localStorage.getItem("userId");
+  const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("Posts");
+  const [about, setAbout] = useState(null);
+  const [mutualFriends,setMutualFriends] = useState([])
 
-        if(!response.ok){
-            throw new Error("failed to fetch posts")
-        }
+  const getPosts = async () => {
+    const response = await fetch(`http://localhost:8080/post/posts/${user_id}`, {
+      method: "GET",
+      headers: {
+        sessionId,
+        userId
+      }
+    });
 
-        const postReponse = await response.json();
-
-        setPosts(postReponse);
-
-        console.log("posts", postReponse);
+    if (!response.ok) {
+      throw new Error("failed to fetch posts");
     }
 
-    useEffect(() => {
-        if (user_id) {
-            getPosts();
-        }
-    }, [user_id]);
+    const postReponse = await response.json();
+    setPosts(postReponse);
+  };
 
-    const getUser = async () => {
-        const response = await fetch(`http://localhost:8080/user/${user_id}`,{
-            method:"GET",
-            headers:{
-                sessionId:sessionId,
-                userId:userId
-            }
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:8080/user/${user_id}`, {
+      method: "GET",
+      headers: {
+        sessionId,
+        userId
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error("failed to fetch user details");
+    }
+
+    const userResponse = await response.json();
+    setUserDetails(userResponse);
+  };
+
+  useEffect(() => {
+    if (user_id) {
+      getPosts();
+      getUser();
+    }
+  }, [user_id]);
+
+  const checkOrCreateConversation = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/conversations`, {
+        method: "GET",
+        headers: {
+          sessionId,
+          userId
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch conversations");
+      }
+
+      const allConversations = await response.json();
+
+      let existingConvo = null;
+
+      for (const convo of allConversations) {
+        const res = await fetch(`http://localhost:8080/conversation-participants/${convo.conversationId}`, {
+          headers: {
+            sessionId,
+            userId
+          }
+        });
+
+        if (!res.ok) continue;
+
+        const participants = await res.json();
+
+        const isOther = participants.some(p => p.userId === parseInt(user_id));
+        const isSelf = participants.some(p => p.userId === parseInt(userId));
+
+        if (isOther && isSelf) {
+          existingConvo = convo;
+          break;
+        }
+      }
+
+      if (existingConvo) {
+        navigate("/chats", { state: { conversationId: existingConvo.conversationId } });
+        return;
+      }
+
+      // Otherwise create a new conversation
+      await createConversation();
+
+    } catch (err) {
+      console.error("Error:", err.message);
+    }
+  };
+
+  const createConversation = async () => {
+    const conversation = {
+      createdAt: Date.now(),
+      isGroup: false,
+      creatorId: userId
+    };
+
+    const response = await fetch("http://localhost:8080/conversations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        sessionId,
+        userId
+      },
+      body: JSON.stringify(conversation)
+    });
+
+    if (!response.ok) {
+      throw new Error("Unable to create conversation");
+    }
+
+    const convo = await response.json();
+
+    await addParticipant(convo.conversationId, userId);
+    await addParticipant(convo.conversationId, user_id);
+
+    navigate("/chats", { state: { conversationId: convo.conversationId } });
+  };
+
+  const addParticipant = async (convoId, uid) => {
+    const participant = {
+      conversationId: convoId,
+      userId: uid,
+      joinedAt: Date.now(),
+      isAdmin: uid === userId
+    };
+
+    const response = await fetch("http://localhost:8080/conversation-participants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        sessionId,
+        userId
+      },
+      body: JSON.stringify(participant)
+    });
+
+    if (!response.ok) {
+      throw new Error("Unable to add participant");
+    }
+
+    const data = await response.json();
+    console.log("Participant added:", data);
+  };
+
+  const getBio = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/bio/${user_id}`, {
+          method: "GET",
+          headers: {
+            userId: userId,
+            sessionId: sessionId,
+          },
         });
   
-        if(!response.ok){
-            throw new Error("failed to fetch user details");
+        if (!response.ok) {
+          throw new Error("Failed to fetch bio");
         }
   
-        const userResponse = await response.json();
-        setUserDetails(userResponse);
-        console.log(userResponse);
-    }
+        const text = await response.text();
+  
+        // If response body is empty, don't parse it as JSON
+        if (!text) {
+          setAbout(null);
+          return;
+        }
+  
+        const bioResponse = JSON.parse(text);
+        setAbout(bioResponse);
+        console.log("Bio:", bioResponse);
+      } catch (error) {
+        console.error("getBio error:", error.message);
+      }
+    };
   
     useEffect(() => {
-        getUser();
-      }, [user_id]);
+      getBio();
+    }, [userId]);
 
-    return(
-        <div className="profil-cont">
-            <div className="cover-pro">
-              <img src="http://localhost:8080/uploads/1744261726947_beach.jpg" alt="cover" className="cover-image" />
-            <div className="profile-pic-wrapper">
-              <img src={userDetails?.profile_img_url} alt="profile" className="profile-pic" />
-            </div>
-            </div>
-            <div className="pro-username">
-                <div className="pro-username-text">
-                    <div className="pro-username-text-1">{userDetails?.name}</div>
-                    <div className="pro-username-text-2">
-                        <div>1,120 friends</div>
-                        <div>400 mutuals</div>
-                    </div>
-                </div>
-                <div className="buttons">
-                    <button>friend</button>
-                    <button>message</button>    
-                </div>
-            </div>
-            <div className="entities">
-                <div className="entity">Post</div>
-                <div className="entity">About</div>
-                <div className="entity">Photos</div>
-                <div className="entity">Friends</div>
-            </div>
-            <div className="user-contents">
-                <div className="user-info"></div>
-                <div className="user-posts">
-                    {posts.map((item) => {
-                        return(
-                            <Post postItem={item}/>
-                        )
-                    })}
-                </div>
-            </div>
+    const getMutualsFriends = async () => {
+      const response = await fetch(`http://localhost:8080/friendship/mutual-friends/${user_id}`,{
+       method:"GET",
+       headers:{
+         sessionId:sessionId,
+         userId:userId
+       }
+      });
+ 
+      if(!response.ok){
+       throw new Error("failed to fetch")
+      }
+ 
+      const mutualResponse = await response.json();
+      setMutualFriends(mutualResponse);
+ 
+      console.log(mutualResponse)
+   }
+ 
+   useEffect(() => {
+     getMutualsFriends()
+   },[])
+   return (
+    <div className="profil-cont">
+      <div className="cover-pro">
+        <img
+          src="http://localhost:8080/uploads/1744261726947_beach.jpg"
+          alt="cover"
+          className="cover-image"
+        />
+        <div className="profile-pic-wrapper">
+          <img
+            src={userDetails?.profile_img_url}
+            alt="profile"
+            className="profile-pic"
+          />
         </div>
-    )
+      </div>
+      <div className="pro-username">
+        <div className="pro-username-text">
+          <div className="pro-username-text-1">{userDetails?.name}</div>
+          <div className="pro-username-text-2">
+            <div>1,120 friends</div>
+            {mutualFriends.length > 0 && (
+              <div>{mutualFriends.length} mutual friends</div>
+            )}
+          </div>
+        </div>
+        <div className="buttons">
+          <button>friend</button>
+          <button onClick={checkOrCreateConversation}>message</button>
+        </div>
+      </div>
+      <div className="entities">
+        {["Posts", "About", "Photos", "Friends"].map((tab) => (
+          <div
+            key={tab}
+            className={`entity ${activeTab === tab ? "active" : ""}`}
+            onClick={() => setActiveTab(tab)}
+          >
+            {tab}
+          </div>
+        ))}
+      </div>
+  
+      <div className="user-contents">
+        {activeTab === "Posts" && (
+          <>
+            <div className="user-info">
+              {about?.workPlace && (
+                <div className="info-display">
+                  <div className="info-display-icon1">
+                    <SiWorkplace />
+                  </div>
+                  <div className="info-display-content">{about?.workPlace}</div>
+                </div>
+              )}
+              {about?.secondarySchool && (
+                <div className="info-display1">
+                  <div className="info-display-icon1">
+                    <RiSchoolLine />
+                  </div>
+                  <div className="info-display-content">
+                    {about?.secondarySchool}
+                  </div>
+                </div>
+              )}
+              {about?.university && (
+                <div className="info-display1">
+                  <div className="info-display-icon1">
+                    <LiaUniversitySolid />
+                  </div>
+                  <div className="info-display-content">{about?.university}</div>
+                </div>
+              )}
+              {about?.currentCity && (
+                <div className="info-display1">
+                  <div className="info-display-icon1">
+                    <IoHomeOutline />
+                  </div>
+                  <div className="info-display-content">{about?.currentCity}</div>
+                </div>
+              )}
+              {about?.homeTown && (
+                <div className="info-display1">
+                  <div className="info-display-icon1">
+                    <CiLocationOn />
+                  </div>
+                  <div className="info-display-content">{about?.homeTown}</div>
+                </div>
+              )}
+              {about?.relationShipStatus && (
+                <div className="info-display1">
+                  <div className="info-display-icon1">
+                    <GiRelationshipBounds />
+                  </div>
+                  <div className="info-display-content">
+                    {about?.relationShipStatus}
+                  </div>
+                </div>
+              )}
+            </div>
+  
+            <div className="user-posts">
+              {posts.map((item) => (
+                <Post postItem={item} key={item.postId} />
+              ))}
+            </div>
+          </>
+        )}
+  
+        {activeTab === "About" && (
+          <div className="about">
+            <div className="about-side1">About</div>
+            <div className="about-side2">
+              {about?.workPlace && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <SiWorkplace />
+                  </div>
+                  <div className="info-display-content">
+                    Works at{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.workPlace}</span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.secondarySchool && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <RiSchoolLine />
+                  </div>
+                  <div className="info-display-content">
+                    Went to{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.secondarySchool}</span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.university && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <LiaUniversitySolid />
+                  </div>
+                  <div className="info-display-content">
+                    Studied at{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.university}</span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.currentCity && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <IoHomeOutline />
+                  </div>
+                  <div className="info-display-content">
+                    Lives in{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.currentCity}</span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.homeTown && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <CiLocationOn />
+                  </div>
+                  <div className="info-display-content">
+                    From{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.homeTown}</span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.relationShipStatus && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <GiRelationshipBounds />
+                  </div>
+                  <div className="info-display-content">
+                    Relationship status{" "}
+                    <span style={{ fontWeight: "bold" }}>
+                      {about.relationShipStatus}
+                    </span>
+                  </div>
+                </div>
+              )}
+  
+              {about?.gender && (
+                <div className="info-display">
+                  <div className="info-display-icon">
+                    <SiWorkplace />
+                  </div>
+                  <div className="info-display-content">
+                    Gender{" "}
+                    <span style={{ fontWeight: "bold" }}>{about.gender}</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+  
+        {activeTab === "Photos" && <div>Photos content</div>}
+        {activeTab === "Friends" && <div>Friends content</div>}
+      </div>
+    </div>
+  );
+  
 }
