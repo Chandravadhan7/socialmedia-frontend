817d4f8227976c7ebbf99b42073640fcbf7c31cf
Chandravadhan <m.chandravadhan@gmail.com>
2025-05-17 22:21:46 +0530

    Add FriendRequestCard component with mutual friends fetch and display functionality

--- src/components/friendRequestCard/friendrequestCard.jsx
+++ src/components/friendRequestCard/friendrequestCard.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import "./friendRequestCard.css"
 export default function FriendRequestCard({item}){
+    const sessionId = localStorage.getItem("sessionId");
+  const userId = localStorage.getItem("userId");
+  const [mutualFriends,setMutualFriends] = useState([])
+ const getMutualsFriends = async () => {
+      
 
+     const response = await fetch(`http://localhost:8080/friendship/mutual-friends/${item?.userId}`,{
+      method:"GET",
+      headers:{
+        sessionId:sessionId,
+        userId:userId
+      }
+     });
+
+     if(!response.ok){
+      throw new Error("failed to fetch")
+     }
+
+     const mutualResponse = await response.json();
+     setMutualFriends(mutualResponse);
+
+     console.log(mutualResponse)
+  }
+
+  useEffect(() => {
+    getMutualsFriends()
+  },[])
     return(
         <div className="request-card">
             <div className="request-pic-cont">
               <img src={item?.profile_img_url} className="request-pic"/>
             </div>
             <div className="request-name">
-                {item?.name}
+                <div className="request-name-user">{item?.name}</div>
+                <div style={{color:"#fff"}}>{mutualFriends.length} mutual Friends</div>
             </div>
             <div className="request-btns">
                 <button>confrim</button>
