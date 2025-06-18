666de287ef0a0f84dfe960704f71653aa0420b55
Chandravadhan <m.chandravadhan@gmail.com>
2025-06-18 22:55:59 +0530

    Remove unused code for comment section

--- src/components/comments/comment.jsx
+++ src/components/comments/comment.jsx
@@ -8,8 +8,7 @@ export default function Comment({ comment }) {
   let [replies, setReplies] = useState([]);
   let [showReplies, setShowReplies] = useState(false);
   let [reply, setReply] = useState("");
-  const [userDetails,setUserDetails] = useState(null);
-
+  const [userDetails, setUserDetails] = useState(null);
 
   const getRelativeTime = (epoch) => {
     return formatDistanceToNowStrict(new Date(epoch), { addSuffix: true });
@@ -62,80 +61,99 @@ export default function Comment({ comment }) {
   };
 
   const getUser = async () => {
-        const response = await fetch(`http://localhost:8080/user/${comment?.userId}`,{
-            method:"GET",
-            headers:{
-                sessionId:sessionId,
-                userId:userId
-            }
-        });
-  
-        if(!response.ok){
-            throw new Error("failed to fetch user details");
-        }
-  
-        const userResponse = await response.json();
-        setUserDetails(userResponse);
-        console.log(userResponse);
+    const response = await fetch(
+      `http://localhost:8080/user/${comment?.userId}`,
+      {
+        method: "GET",
+        headers: {
+          sessionId: sessionId,
+          userId: userId,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("failed to fetch user details");
     }
-  
-    useEffect(() => {
-      getUser();
-    }, []);
+
+    const userResponse = await response.json();
+    setUserDetails(userResponse);
+    console.log(userResponse);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
-   <div className="whole-cont">
-    <div className="comment-cont">
-      <div className="comment-cont-s1">
-        <div className="comment-cont-s1-img-cont">
-        <img src={"https://i.ibb.co/67HWYXmq/icons8-user-96.png"} className="post-pro-pic" alt="profile"/>
-        </div>
-      </div>
-      <div className="comment-cont-s2">
-        <div className="comment-cont-s2-1">
-          <div className="comm-username">
-           <div style={{fontSize:"105%",fontWeight:"555"}}>{userDetails?.name}</div> 
-            <div className="com-time">{getRelativeTime(comment?.createdAt)}</div>
-          </div>
-          <div className="comm-content">{comment?.content}</div>
-        </div>
-        <div className="comment-cont-s2-2">
-          <div className="com-like">like</div>
-          <div
-            className="com-reply"
-            onClick={() => setShowReplies(!showReplies)}
-            style={{ cursor: "pointer" }}
-          >
-            reply
-          </div>
-        </div>
-      </div>
-      {/* Render replies and reply input when showReplies is true */}
-    </div>
-    {showReplies && (
-        <div className="reply-section">
-          <div className={replies.length > 0?"reply-child":"d-none"}>
-            {replies.map((item) => {
-                return(
-                    <Comment comment={item}/>
-                )
-            })}
-          </div>
-          <div className="add-reply">
-          <div className= "pic-cont">
-          <img
+    <div className="whole-cont">
+      <div className="comment-cont">
+        <div className="comment-cont-s1">
+          <div className="comment-cont-s1-img-cont">
+            <img
               src={"https://i.ibb.co/67HWYXmq/icons8-user-96.png"}
               className="post-pro-pic"
               alt="profile"
             />
           </div>
-          <input placeholder="reply to user" onChange={(e)=>setReply(e.target.value)} value={reply} className="comment-bar"/>
-            <button onClick={() => { 
-            addReply(); 
-            setTimeout(() => {
-            window.location.reload();
-            }, 500);
-            }} className="reply-btn">Post</button>
+        </div>
+        <div className="comment-cont-s2">
+          <div className="comment-cont-s2-1">
+            <div className="comm-username">
+              <div style={{ fontSize: "105%", fontWeight: "555" }}>
+                {userDetails?.name}
+              </div>
+              <div className="com-time">
+                {getRelativeTime(comment?.createdAt)}
+              </div>
+            </div>
+            <div className="comm-content">{comment?.content}</div>
+          </div>
+          <div className="comment-cont-s2-2">
+            <div className="com-like">like</div>
+            <div
+              className="com-reply"
+              onClick={() => setShowReplies(!showReplies)}
+              style={{ cursor: "pointer" }}
+            >
+              reply
+            </div>
+          </div>
+        </div>
+        {/* Render replies and reply input when showReplies is true */}
+      </div>
+      {showReplies && (
+        <div className="reply-section">
+          <div className={replies.length > 0 ? "reply-child" : "d-none"}>
+            {replies.map((item) => {
+              return <Comment comment={item} />;
+            })}
+          </div>
+          <div className="add-reply">
+            <div className="pic-cont">
+              <img
+                src={"https://i.ibb.co/67HWYXmq/icons8-user-96.png"}
+                className="post-pro-pic"
+                alt="profile"
+              />
+            </div>
+            <input
+              placeholder="reply to user"
+              onChange={(e) => setReply(e.target.value)}
+              value={reply}
+              className="comment-bar"
+            />
+            <button
+              onClick={() => {
+                addReply();
+                setTimeout(() => {
+                  window.location.reload();
+                }, 500);
+              }}
+              className="reply-btn"
+            >
+              Post
+            </button>
           </div>
         </div>
       )}
