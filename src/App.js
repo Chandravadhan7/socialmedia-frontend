5c255309f15b4a1d24f57e01d999f659957bcf93
Chandravadhan <m.chandravadhan@gmail.com>
2025-07-02 22:40:40 +0530

    written a function to exclude the header in login and sighup pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import LoginPage from './pages/loginpage';
 import Home from './pages/home';
 import Header from './components/header/header';
@@ -14,25 +14,29 @@ import Profile from './pages/profile';
 import SignUp from './pages/signUp';
 import ProtectedRoute from './components/protectedRoute';
 
-export default function App(){
-  return(
-    <div className='App'>
-      <Header/>
-       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/*" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
-        <Route path="/friends" element={<Friends/>}/>
-        <Route path="/friendrequest" element={<FriendRequestPage/>}/>
-        <Route path="/suggestions" element={<SuggestionsPage/>}/>
-        <Route path="/friends/list" element={<AllFriends/>}/>
-        {/* <Route path="profile/:userId" element={<UserProfile/>}/> */}
-        <Route path="/chats" element={<Conversations/>}/>
-        <Route path="/profile/:user_id" element={<FriendsProfile/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/friends/homecontent" element={<Friends />} />
-       </Routes>
+export default function App() {
+  const location = useLocation();
+  const hideHeaderPaths = ['/login', '/signup'];
 
+  return (
+    <div className='App'>
+      {/* Conditionally render Header except on login and signup pages */}
+      {!hideHeaderPaths.includes(location.pathname) && <Header />}
+
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/*" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/friendrequest" element={<FriendRequestPage />} />
+        <Route path="/suggestions" element={<SuggestionsPage />} />
+        <Route path="/friends/list" element={<AllFriends />} />
+        {/* <Route path="profile/:userId" element={<UserProfile />} /> */}
+        <Route path="/chats" element={<Conversations />} />
+        <Route path="/profile/:user_id" element={<FriendsProfile />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/friends/homecontent" element={<Friends />} />
+      </Routes>
     </div>
-  )
+  );
 }
